6484006dd519a38d59712362802759ab1f319edf
wweir <wweir@foxmail.com>
2024-10-20 11:58:43 +0800

    fix: log level

--- enhance.go
+++ enhance.go
@@ -2,6 +2,7 @@ package deferlog
 
 import (
 	"context"
+	"os"
 )
 
 func InfoWarn(err error, msg string, args ...any) {
@@ -20,28 +21,30 @@ func InfoWarnContext(ctx context.Context, err error, msg string, args ...any) {
 
 func InfoError(err error, msg string, args ...any) {
 	if err != nil {
-		Default().Warn(msg, append(args, "err", err)...)
+		Default().Error(msg, append(args, "err", err)...)
 	}
 	Default().Info(msg, args...)
 }
 
 func InfoErrorContext(ctx context.Context, err error, msg string, args ...any) {
 	if err != nil {
-		Default().WarnContext(ctx, msg, append(args, "err", err)...)
+		Default().ErrorContext(ctx, msg, append(args, "err", err)...)
 	}
 	Default().InfoContext(ctx, msg, args...)
 }
 
 func InfoFatal(err error, msg string, args ...any) {
 	if err != nil {
-		Default().Warn(msg, append(args, "err", err)...)
+		Default().Error(msg, append(args, "err", err)...)
+		os.Exit(1)
 	}
 	Default().Info(msg, args...)
 }
 
 func InfoFatalContext(ctx context.Context, err error, msg string, args ...any) {
 	if err != nil {
-		Default().WarnContext(ctx, msg, append(args, "err", err)...)
+		Default().ErrorContext(ctx, msg, append(args, "err", err)...)
+		os.Exit(1)
 	}
 	Default().InfoContext(ctx, msg, args...)
 }
@@ -50,40 +53,42 @@ func DebugWarn(err error, msg string, args ...any) {
 	if err != nil {
 		Default().Warn(msg, append(args, "err", err)...)
 	}
-	Default().Info(msg, args...)
+	Default().Debug(msg, args...)
 }
 
 func DebugWarnContext(ctx context.Context, err error, msg string, args ...any) {
 	if err != nil {
 		Default().WarnContext(ctx, msg, append(args, "err", err)...)
 	}
-	Default().InfoContext(ctx, msg, args...)
+	Default().DebugContext(ctx, msg, args...)
 }
 
 func DebugError(err error, msg string, args ...any) {
 	if err != nil {
-		Default().Warn(msg, append(args, "err", err)...)
+		Default().Error(msg, append(args, "err", err)...)
 	}
-	Default().Info(msg, args...)
+	Default().Debug(msg, args...)
 }
 
 func DebugErrorContext(ctx context.Context, err error, msg string, args ...any) {
 	if err != nil {
-		Default().WarnContext(ctx, msg, append(args, "err", err)...)
+		Default().ErrorContext(ctx, msg, append(args, "err", err)...)
 	}
-	Default().InfoContext(ctx, msg, args...)
+	Default().DebugContext(ctx, msg, args...)
 }
 
 func DebugFatal(err error, msg string, args ...any) {
 	if err != nil {
-		Default().Warn(msg, append(args, "err", err)...)
+		Default().Error(msg, append(args, "err", err)...)
+		os.Exit(1)
 	}
-	Default().Info(msg, args...)
+	Default().Debug(msg, args...)
 }
 
 func DebugFatalContext(ctx context.Context, err error, msg string, args ...any) {
 	if err != nil {
-		Default().WarnContext(ctx, msg, append(args, "err", err)...)
+		Default().ErrorContext(ctx, msg, append(args, "err", err)...)
+		os.Exit(1)
 	}
-	Default().InfoContext(ctx, msg, args...)
+	Default().DebugContext(ctx, msg, args...)
 }
